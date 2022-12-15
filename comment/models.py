6865934e780b42e27e1f0e6eeba57aa95ecdfdd3
zOmegad <omegad@protonmail.com>
2022-12-15 10:47:34 +0100

    Add: User FK to Comment

--- comment/models.py
+++ comment/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 from post.models import Post
 
 class Comment(models.Model):
     body = models.TextField(max_length=200)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user')
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
