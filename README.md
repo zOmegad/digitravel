69adfbb84210c936484b3e269c71f5459a2e0829
zOmegad <omegad@protonmail.com>
2022-12-16 14:35:37 +0100

    Add: Upvotes model connected to Post

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # digitravel
+
+# upvotes en back
+p = Post.objects.get(id=2)
+i = p.upvotes.filter(upvoted=True)
+for t in i:
+    print(t.user)
--- post/models.py
+++ post/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from upvote.models import Upvote
 
 class Post(models.Model):
     city = models.CharField(max_length=40, unique=True)
     country = models.CharField(max_length=40)
     language = models.CharField(max_length=40)
+    upvotes = models.ManyToManyField(Upvote, related_name="upvotes")
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
-        return str(self.id) + ": " + self.city
+        return "Id: {} | City: {} | Upvotes: {} ".format(self.id, self.city, self.upvotes.filter(upvoted=True).count())
 
 class Photo(models.Model):
     image = models.ImageField()
