10993c50beb92ae0b52793fe55bfdafdf65de7a1
zOmegad <omegad@protonmail.com>
2022-12-19 18:07:20 +0100

    Add: Average score from Reviews

--- post/views.py
+++ post/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Avg
 from post.models import Post
 from comment.models import Comment
 from review.models import Review
@@ -11,4 +12,9 @@ def show(request, post_id):
     item = Post.objects.get(pk=post_id)
     comments = Comment.objects.filter(post_id=post_id)
     reviews = Review.objects.filter(post_id=post_id)
-    return render(request, 'post/show.html', {'post': item, 'comments': comments, 'reviews': reviews})
+    post_score = reviews.aggregate(avg_rating=Avg('score'))
+    return render(request, 'post/show.html', 
+        {'post': item, 
+        'comments': comments, 
+        'reviews': reviews, 
+        'post_score': post_score})
