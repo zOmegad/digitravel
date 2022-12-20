fdaebb110cd58952bcbebd43ae6e945d8877f878
zOmegad <omegad@protonmail.com>
2022-12-20 10:43:32 +0100

    Add: Profile view + template

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 
 from .views import SignUpView
+from . import views
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/", views.profile, name="profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -7,3 +9,7 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+@login_required
+def profile(request):
+    return render(request, 'profile/profile.html')
