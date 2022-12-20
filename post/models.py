6a62064aeffa6b00c2ea26e0c79ec1c8914c8456
zOmegad <omegad@protonmail.com>
2022-12-20 14:52:23 +0100

    Remove field upvotes from post

--- post/models.py
+++ post/models.py
@@ -5,7 +5,6 @@ class Post(models.Model):
     city = models.CharField(max_length=40, unique=True)
     country = models.CharField(max_length=40)
     language = models.CharField(max_length=40)
-    upvotes = models.ManyToManyField(Upvote, related_name="upvotes")
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
