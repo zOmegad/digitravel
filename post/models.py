e4c3ec5433aa1ec22255e9b37c79656a76a31d12
zOmegad <omegad@protonmail.com>
2022-12-20 11:13:01 +0100

    Edit: Post_score refactored in post.models for better calls

--- post/models.py
+++ post/models.py
@@ -11,6 +11,14 @@ class Post(models.Model):
         """String for representing the MyModelName object (in Admin site etc.)."""
         return "Id: {} | City: {} | Upvotes: {} ".format(self.id, self.city, self.upvotes.filter(upvoted=True).count())
 
+    @property
+    def post_score(self):
+        from django.db.models import Avg
+        from review.models import Review
+        my_review = Review.objects.filter(post_id=self.id)
+        post_score = my_review.aggregate(avg_rating=Avg('score'))
+        return post_score
+
 class Photo(models.Model):
     image = models.ImageField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- post/views.py
+++ post/views.py
@@ -12,9 +12,7 @@ def show(request, post_id):
     item = Post.objects.get(pk=post_id)
     comments = Comment.objects.filter(post_id=post_id)
     reviews = Review.objects.filter(post_id=post_id)
-    post_score = reviews.aggregate(avg_rating=Avg('score'))
     return render(request, 'post/show.html', 
         {'post': item, 
         'comments': comments, 
-        'reviews': reviews, 
-        'post_score': post_score})
+        'reviews': reviews})
