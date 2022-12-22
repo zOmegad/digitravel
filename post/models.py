895df33d923e4d1928e42dcc02627c309f85e926
zOmegad <omegad@protonmail.com>
2022-12-22 15:10:02 +0100

    Add: currency to Posts + script updated

--- post/models.py
+++ post/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     population = models.IntegerField(default=0)
     longitude = models.FloatField(default=0)
     latitude = models.FloatField(default=0)
+    currency = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
