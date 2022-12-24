fc2272cbde76b8147a1c981c81974f6070308746
zOmegad <omegad@protonmail.com>
2022-12-24 15:15:31 +0100

    Edit: Autologin after sign up

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import login
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("index") 
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+            valid = super().form_valid(form)
+            login(self.request, self.object)
+            return valid
 
 @login_required
 def profile(request):
