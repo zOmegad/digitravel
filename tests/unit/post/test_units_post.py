299cf6b0dd8e075a1a1cf5d9d3e9a3d6d42e9200
zOmegad <omegad@protonmail.com>
2022-12-24 15:14:28 +0100

    Add: Units testsé

---
+++ tests/unit/post/test_units_post.py
@@ -0,0 +1,32 @@
+from django.urls import reverse
+from django.test import TestCase
+
+from post.models import Post
+
+
+class IndexPageTestCase(TestCase):
+    def test_index_page_returns_200(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+    
+class ShowPageTestCase(TestCase):
+    def setUp(self):
+        Post.objects.create(
+            city = "Paris",
+            country = "France",
+            language = "FRA",
+            population = 10000,
+            longitude = 1.102,
+            latitude = 2.021,
+            currency = "EUR",
+            continent = "Europe",
+            region = "West Europe")
+        self.post = Post.objects.first()
+
+    def test_show_with_product_returns_200(self):
+        response = self.client.get(reverse('show', args=[self.post.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_show_data_is_correct(self):
+        response = self.client.get(reverse('show', args=[self.post.id]))
+        self.assertEqual(response.post, True)
