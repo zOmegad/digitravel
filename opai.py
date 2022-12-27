3863c38e65ebebc3a32bd813b3c53b17e452aa04
zOmegad <omegad@protonmail.com>
2022-12-27 17:18:07 +0100

    Edit: Images

--- opai.py
+++ opai.py
@@ -7,6 +7,16 @@ load_dotenv('.env')
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+response = openai.Image.create(
+  prompt="A picture of Paris in France",
+  n=1,
+  size="1024x1024"
+)
+image_url = response['data'][0]['url']
+
+print(image_url)
+"""
 response = openai.Completion.create(
   model="text-curie-001",
   prompt="Q: Describe me Paris as if i were a tourist",
@@ -18,3 +28,4 @@ response = openai.Completion.create(
 )
 
 print(response["choices"][0]["text"])
+"""
