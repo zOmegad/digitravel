46c8f5f771bcb2a7dc7ea65121488000e5cf7eab
zOmegad <omegad@protonmail.com>
2023-01-05 17:56:00 +0100

    Add: user integration test

---
+++ tests/test_integration/test_integration_user.py
@@ -0,0 +1,33 @@
+from django.urls import reverse
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
+class SignUpTestCase(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'Rg^K,4ybA"3*PdP[>e^s8'
+
+    def test_signup_form(self):
+        response = self.client.post(reverse('signup'), data={
+            'username': self.username,
+            'password1': self.password,
+            'password2': self.password
+        })
+        self.assertEqual(response.wsgi_request.user.username, self.username)
+        self.assertEqual(response.status_code, 302) # return 200 if password fails
+
+    def test_login_form(self):
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        response = self.client.post(reverse('login'), data={
+            'username': self.username,
+            'password': self.password
+        })
+        self.assertEqual(response.url, '/accounts/profile/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.wsgi_request.user.username, self.username)
+    
+    def test_logout(self):
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.wsgi_request.user.username, '')
