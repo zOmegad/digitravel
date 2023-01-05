3c9503dfe91dc54e782b47a2536cc23fa43c6673
zOmegad <omegad@protonmail.com>
2023-01-05 15:22:18 +0100

    Add: Integration test signup

---
+++ tests/integration/sign_up/test_integration_sign_up.py
@@ -0,0 +1,20 @@
+from django.urls import reverse
+from django.test import TestCase
+
+class SignUpTestCase(TestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'Rg^K,4ybA"3*PdP[>e^s8'
+
+    def test_signup_returns_page(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_form(self):
+        response = self.client.post(reverse('signup'), data={
+            'username': self.username,
+            'password1': self.password,
+            'password2': self.password
+        })
+        # return 200 if password fails
+        self.assertEqual(response.status_code, 302)
